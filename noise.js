18dc9ac97970ac6e73789ff164c9e4e720f580a3
Stephanie Marchante <smarcha04@gmail.com>
2019-05-28 15:47:39 -0700

    added functionality to play sound on keyboard press of corresponding letter

--- noise.js
+++ noise.js
@@ -3,12 +3,26 @@ $(document).ready( function() {
   $( ".note.c" ).click(function() {
     let sound = document.getElementById("cAudio");
     sound.play();
-    // alert( "Handler for .click() called." );
   });
 
+  $('body').keydown((event) => {
+    let sound = document.getElementById("cAudio");
+    if (event.key === 'c') {
+     sound.play();
+    }
+  });
+
+  
   $( ".note.d" ).click(function() {
     let sound = document.getElementById("dAudio");
     sound.play();
+  });
+
+  $('body').keydown((event) => {
+    let sound = document.getElementById("dAudio");
+    if (event.key === 'd') {
+     sound.play();
+    }
   });
 
   $( ".note.e" ).click(function() {
@@ -16,9 +30,23 @@ $(document).ready( function() {
     sound.play();
   });
 
+  $('body').keydown((event) => {
+    let sound = document.getElementById("eAudio");
+    if (event.key === 'e') {
+     sound.play();
+    }
+  });
+
   $( ".note.f" ).click(function() {
     let sound = document.getElementById("fAudio");
     sound.play();
+  });
+
+  $('body').keydown((event) => {
+    let sound = document.getElementById("fAudio");
+    if (event.key === 'f') {
+     sound.play();
+    }
   });
 
   $( ".note.g" ).click(function() {
@@ -26,14 +54,35 @@ $(document).ready( function() {
     sound.play();
   });
 
+  $('body').keydown((event) => {
+    let sound = document.getElementById("gAudio");
+    if (event.key === 'g') {
+     sound.play();
+    }
+  });
+
   $( ".note.a" ).click(function() {
     let sound = document.getElementById("aAudio");
     sound.play();
   })
 
+  $('body').keydown((event) => {
+    let sound = document.getElementById("aAudio");
+    if (event.key === 'a') {
+     sound.play();
+    }
+  });
+
   $( ".note.b" ).click(function() {
     let sound = document.getElementById("bAudio");
     sound.play();
+  });
+
+  $('body').keydown((event) => {
+    let sound = document.getElementById("bAudio");
+    if (event.key === 'b') {
+     sound.play();
+    }
   });
 
 });
