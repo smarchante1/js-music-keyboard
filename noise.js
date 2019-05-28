015047053273b612cfac31980e489fe74a2cf5a8
Stephanie Marchante <smarcha04@gmail.com>
2019-05-28 15:38:59 -0700

    made sounds from the keyboard yay

--- noise.js
+++ noise.js
@@ -1,3 +1,39 @@
 $(document).ready( function() {
   // your code here
+  $( ".note.c" ).click(function() {
+    let sound = document.getElementById("cAudio");
+    sound.play();
+    // alert( "Handler for .click() called." );
+  });
+
+  $( ".note.d" ).click(function() {
+    let sound = document.getElementById("dAudio");
+    sound.play();
+  });
+
+  $( ".note.e" ).click(function() {
+    let sound = document.getElementById("eAudio");
+    sound.play();
+  });
+
+  $( ".note.f" ).click(function() {
+    let sound = document.getElementById("fAudio");
+    sound.play();
+  });
+
+  $( ".note.g" ).click(function() {
+    let sound = document.getElementById("gAudio");
+    sound.play();
+  });
+
+  $( ".note.a" ).click(function() {
+    let sound = document.getElementById("aAudio");
+    sound.play();
+  })
+
+  $( ".note.b" ).click(function() {
+    let sound = document.getElementById("bAudio");
+    sound.play();
+  });
+
 });
